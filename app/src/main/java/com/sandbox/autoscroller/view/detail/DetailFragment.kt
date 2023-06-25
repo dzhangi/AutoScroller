c7b53621b7dda83dd1f21094f7ffcefc8dc64766
pkonstantinov <dzhangitau@gmail.com>
2023-06-25 07:08:15 -0700

    Implement detail view model and fragment

--- app/src/main/java/com/sandbox/autoscroller/view/detail/DetailFragment.kt
+++ app/src/main/java/com/sandbox/autoscroller/view/detail/DetailFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sandbox.autoscroller.databinding.FragmentDetailBinding
 import com.sandbox.autoscroller.di.module.viewmodel.ViewModelProviderFactory
+import com.sandbox.autoscroller.view.LoadingState
+import com.sandbox.autoscroller.viewmodel.detail.AllInfo
 import com.sandbox.autoscroller.viewmodel.detail.DetailViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class DetailFragment : DaggerFragment() {
@@ -18,6 +24,10 @@ class DetailFragment : DaggerFragment() {
     lateinit var factory: ViewModelProviderFactory
     private lateinit var viewModel: DetailViewModel
 
+    private val args: DetailFragmentArgs by lazy {
+        DetailFragmentArgs.fromBundle(requireArguments())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,5 +36,37 @@ class DetailFragment : DaggerFragment() {
         viewModel = ViewModelProvider(this, factory)[DetailViewModel::class.java]
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.loadingState.collect { state ->
+                when (state) {
+                    is LoadingState.Success -> {
+                        binding.detailProgressPb.visibility = View.GONE
+
+                        val carDetails = (state.data as AllInfo).carDetails
+                        val posts = (state.data as AllInfo).posts.posts
+
+                        val adapter = DetailAdapter(carDetails, posts)
+                        binding.detailRv.adapter = adapter
+                        binding.detailRv.layoutManager = LinearLayoutManager(requireContext())
+                    }
+
+                    is LoadingState.Loading -> binding.detailProgressPb.visibility = View.VISIBLE
+                    is LoadingState.Error -> {
+                        binding.detailProgressPb.visibility = View.GONE
+                        Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getInfo(args.carId)
     }
 }
--- app/src/main/java/com/sandbox/autoscroller/viewmodel/detail/DetailViewModel.kt
+++ app/src/main/java/com/sandbox/autoscroller/viewmodel/detail/DetailViewModel.kt
@@ -1,8 +1,40 @@
 package com.sandbox.autoscroller.viewmodel.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sandbox.autoscroller.model.models.CarDetails
+import com.sandbox.autoscroller.model.models.Posts
+import com.sandbox.autoscroller.model.repository.AutoRepository
+import com.sandbox.autoscroller.view.LoadingState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class DetailViewModel @Inject constructor(): ViewModel() {
+class DetailViewModel @Inject constructor(
+    private val repository: AutoRepository
+) : ViewModel() {
+    private val _loadingState: MutableStateFlow<LoadingState> =
+        MutableStateFlow(LoadingState.Loading)
+    val loadingState = _loadingState.asStateFlow()
 
+    fun getInfo(carId: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _loadingState.value = LoadingState.Loading
+            val details = async { repository.getCarDetails(carId) }
+            val posts = async { repository.getCarPosts(carId) }
+            val allInfo = awaitAll(details, posts)
+                .runCatching { AllInfo(get(0) as CarDetails, get(1) as Posts) }
+                .onSuccess { _loadingState.value = LoadingState.Success(it) }
+                .onFailure { _loadingState.value = LoadingState.Error() }
+        }
+    }
 }
+
+data class AllInfo(
+    val carDetails: CarDetails,
+    val posts: Posts
+)
