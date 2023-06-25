f1134dcd60bbe9f9b764a87e7fad8bf86b9fdc6c
pkonstantinov <dzhangitau@gmail.com>
2023-06-25 06:47:02 -0700

    Implement list for feed screen and simple paging

--- app/src/main/java/com/sandbox/autoscroller/view/feed/FeedFragment.kt
+++ app/src/main/java/com/sandbox/autoscroller/view/feed/FeedFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sandbox.autoscroller.databinding.FragmentFeedBinding
 import com.sandbox.autoscroller.di.module.viewmodel.ViewModelProviderFactory
+import com.sandbox.autoscroller.model.models.Car
+import com.sandbox.autoscroller.view.LoadingState
 import com.sandbox.autoscroller.viewmodel.feed.FeedViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -19,6 +25,16 @@ class FeedFragment : DaggerFragment() {
     lateinit var factory: ViewModelProviderFactory
     private lateinit var viewModel: FeedViewModel
 
+    private lateinit var adapter: FeedAdapter
+
+    private val itemClickListener = object : OnItemClickListener {
+        override fun onItemClick(position: Int, item: Any) {
+            val carId = (item as Car).id
+
+            // Navigate to detail fragment
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,5 +43,51 @@ class FeedFragment : DaggerFragment() {
         viewModel = ViewModelProvider(this, factory)[FeedViewModel::class.java]
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        adapter = FeedAdapter()
+        binding.mainRv.adapter = adapter
+        binding.mainRv.layoutManager = layoutManager
+
+        val scrollListener = PaginationScrollListener(layoutManager) {
+            Toast.makeText(requireContext(), "Load more", Toast.LENGTH_SHORT).show()
+            viewModel.getCars()
+        }
+
+        binding.mainRv.addOnScrollListener(scrollListener)
+        adapter.setOnItemClickListener(itemClickListener)
+
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.loadingState.collect() { state ->
+                when (state) {
+                    is LoadingState.Success -> {
+                        binding.mainProgressPb.visibility = View.GONE
+
+                        val cars = state.data as List<Car>
+                        val currentList = adapter.currentList.toMutableList()
+                        val newList = currentList + cars
+                        adapter.submitList(newList)
+
+                        scrollListener.setLoaded()
+                    }
+
+                    is LoadingState.Loading -> binding.mainProgressPb.visibility = View.VISIBLE
+                    is LoadingState.Error -> {
+                        binding.mainProgressPb.visibility = View.GONE
+                        Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getCars()
     }
 }
--- app/src/main/java/com/sandbox/autoscroller/viewmodel/feed/FeedViewModel.kt
+++ app/src/main/java/com/sandbox/autoscroller/viewmodel/feed/FeedViewModel.kt
@@ -1,10 +1,34 @@
 package com.sandbox.autoscroller.viewmodel.feed
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sandbox.autoscroller.model.repository.AutoRepository
+import com.sandbox.autoscroller.view.LoadingState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class FeedViewModel @Inject constructor() : ViewModel() {
-    fun testPrint() {
-        println("FeedViewModel test DI...")
+class FeedViewModel @Inject constructor(
+    private val repository: AutoRepository
+) : ViewModel() {
+    private val _loadingState: MutableStateFlow<LoadingState> =
+        MutableStateFlow(LoadingState.Loading)
+    val loadingState = _loadingState.asStateFlow()
+
+    private var currentPage = 1
+    private var pageSize = 9
+
+    fun getCars() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val cars = repository.getCars(pageSize, currentPage)
+                _loadingState.value = LoadingState.Success(cars)
+                currentPage++
+            } catch (e: Exception) {
+                _loadingState.value = LoadingState.Error()
+            }
+        }
     }
 }
